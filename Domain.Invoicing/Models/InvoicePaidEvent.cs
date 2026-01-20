b7fabd8dba9ad400268152352764c88c6488b95c
adesima <simaadesebastian@gmail.com>
2026-01-20 23:35:35 +0200

    Proiect final!

--- Domain.Invoicing/Models/InvoicePaidEvent.cs
+++ Domain.Invoicing/Models/InvoicePaidEvent.cs
@@ -1,9 +1,9 @@
-﻿namespace Domain.Invoicing.Models;
-
-public record InvoicePaidEvent
-{
-    public Guid InvoiceId { get; init; }
-    public Guid OrderId { get; init; }
-    public required Money Amount { get; init; }
-    public DateTime PaidAt { get; init; }
-}
+﻿// namespace Domain.Invoicing.Models;
+//
+// public record InvoicePaidEvent
+// {
+//     public Guid InvoiceId { get; init; }
+//     public Guid OrderId { get; init; }
+//     public required Money Amount { get; init; }
+//     public DateTime PaidAt { get; init; }
+// }
